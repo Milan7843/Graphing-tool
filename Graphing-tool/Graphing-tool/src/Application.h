583918ec7dc006deeab034f76dbe7de24aa753af
Milan Otten <m.f.otten@student.tudelft.nl>
2022-06-07 21:30:20 +0200

    Add ImGui demo
    Add ImGui demo, to be able to modify it and learn from it

--- Graphing-tool/Graphing-tool/src/Application.h
+++ Graphing-tool/Graphing-tool/src/Application.h
@@ -6,7 +6,6 @@
 #include <GLFW/glfw3.h>
 
 // Matrix math
-
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
@@ -16,6 +15,24 @@
 #include "Shader.h"
 #include "Camera.h"
 #include "Callbacks.h"
+
+// ImGui
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+#include <stdio.h>
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
+#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+
+// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
+// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
+// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
+
 
 class Application
 {
